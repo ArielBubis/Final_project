631e0e2c08b7620f5cc2ee5718a020fc5a76e219
ArielBubis <arielbubis@gmail.com>
2025-04-14 16:01:33 +0300

    Refactor Firestore integration files for improved organization

--- src/firebase/firestore_admin.py
+++
@@ -1,4 +0,0 @@
-from google.cloud import firestore
-
-# Initialize Firestore DB
-admin_db = firestore.Client()
--- src/firebase/firestore_client_services.py
+++
@@ -1,3 +0,0 @@
-from firestore_client import db
-
-# Now you can use `db` to interact with Firestore in this module
